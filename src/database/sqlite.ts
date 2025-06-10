02a9b848a4d28b9cad6dbf8d6d19b52a55609bc7
Lucas Miranda <lucasmsoares72@gmail.com>
2025-06-10 18:44:09 -0300

    Update sqlite.ts

--- src/database/sqlite.ts
+++ src/database/sqlite.ts
@@ -1,9 +1,7 @@
 import sqlite3 from 'sqlite3';
 
-// Abre uma conexão com o banco de dados SQLite (cria um novo banco se não existir)
 const db = new sqlite3.Database('database.db');
 
-// Cria a tabela de usuários se ela não existir
 db.run(`CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
@@ -11,7 +9,6 @@ db.run(`CREATE TABLE IF NOT EXISTS users (
   password TEXT
 )`);
 
-// Função para salvar um usuário no banco de dados
 export function saveUserToDatabase(name: string, email: string, password: string): Promise<number> {
   return new Promise((resolve, reject) => {
     console.log('saveUserToDatabase');
